75a3aa1503bb96042e268b3cb0aee19ef88f740e
Gachanno <vitana2452@gmail.com>
2025-02-27 21:52:37 +0300

    first commit

---
+++ src/Shared/Navlogo/NavLog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './NavLogo.module.scss'
+
+const NavLog = () => {
+  return (
+    <a href="" className={classes.logo}>
+        <img src="../assets/Gamecamo.svg" alt="Перейти на главную старницу" />
+    </a>
+  )
+}
+
+export default NavLog
